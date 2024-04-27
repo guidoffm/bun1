0f3a085010fcbf49755757519fb34f88d66627d3
guidoffm <guidoffm@users.noreply.github.com>
2024-04-27 16:52:17 +0200

    Add Docker

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bun1
+
+## Local Run
+
+    bun --watch server.ts
+
+## Build and run in Docker
+
+    docker compose up --build -d
---
+++ server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// app.use("/api", require("./routes/api"));
+// app.use("/auth", require("./routes/auth"));
+// app.use("/user", require("./routes/user"));
+// app.use("/admin", require("./routes/admin"));
+// app.use("/super", require("./routes/super"));
+// app.use("/test", require("./routes/test"));
+// add express middleware to handle errors  
+app.use((err: any, _req: any, res: any, _next: any) => {
+  console.log(err);
+  res.status(500).send("Something broke!");
+});
+
+// add express custom middleware
+app.use((req: any, _res: any, next: any) => {
+  console.log(`"${req.url}" requested`);
+  next();
+});
+
+app.get("/", (_req, res) => {
+    // throw new Error("BROKEN"); // Express will catch this on its own.
+  res.send({ message: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
