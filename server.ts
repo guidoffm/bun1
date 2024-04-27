3f49bc726a2140bf1dacba93a6659a817d23cd5a
guidoffm <guidoffm@users.noreply.github.com>
2024-04-27 23:04:25 +0200

    Add JWT middleware

--- server.ts
+++ server.ts
@@ -1,4 +1,9 @@
-import express from "express";
+
+import express, { NextFunction, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+import jwt, { DecodeOptions, Jwt, JwtPayload } from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
 
 const app = express();
 const port = 8080;
@@ -13,7 +18,7 @@ app.use(express.static("public"));
 // app.use("/super", require("./routes/super"));
 // app.use("/test", require("./routes/test"));
 // add express middleware to handle errors  
-app.use((err: any, _req: any, res: any, _next: any) => {
+app.use((err: any, _req: any, res: Response, _next: any) => {
   console.log(err);
   res.status(500).send("Something broke!");
 });
@@ -24,16 +29,47 @@ app.use((req: any, _res: any, next: any) => {
   next();
 });
 
-app.get("/healthz", (_req, res) => {
+app.get("/healthz", (_req, res: Response) => {
   res.status(200).send("OK");
 });
 
-app.get("/livez", (_req, res) => {
+app.get("/livez", (_req, res: Response) => {
   res.status(200).send("OK");
 });
 
-app.get("/readyz", (_req, res) => {
+app.get("/readyz", (_req, res: Response) => {
   res.status(200).send("OK");
+});
+
+app.use('/api/', async (req: Request, res: Response, next: NextFunction) => {
+  console.log('JWT middleware');
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) {
+      res.sendStatus(401);
+  } else {
+      try {
+          const jwtoken = jwt.decode(token, { complete: true } as DecodeOptions) as Jwt;
+          const kid = jwtoken.header.kid;
+          // console.log('kid', kid);
+          const iss = (jwtoken.payload as JwtPayload).iss;
+          // console.log('iss', iss);
+          const configResponse = await fetch(`${iss}/.well-known/openid-configuration`);
+          const data = await configResponse.json();
+          const jwksUri = data.jwks_uri;
+          // console.log(jwksUri);
+          const client = jwksClient({ jwksUri: jwksUri, timeout: 3000 });
+          const key = await client.getSigningKey(kid);
+          const signingKey = key.getPublicKey();
+          const tokenData = jwt.verify(token, signingKey);
+          console.log('tokenData', tokenData);
+          res.locals['tokenData'] = tokenData;
+          next();
+      } catch (err) {
+          console.error(err);
+          res.sendStatus(403);
+      }
+  }
 });
 
 app.get("/api/test", (_req, res) => {
